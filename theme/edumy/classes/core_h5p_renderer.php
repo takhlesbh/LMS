4f4a0ed14c1b5deb3dc7af61e96ec64aed05a3bc
root <root@manage.cibafi.org>
2025-06-22 02:55:37 +0000

    INITIAL COMMIT

---
+++ theme/edumy/classes/core_h5p_renderer.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die;
+
+// namespace theme_edumy;
+
+// $targetRenderer = $CFG->dirroot . "/h5p/classes/output/renderer.php";
+// if (file_exists($targetRenderer) {
+//   require_once($targetRenderer);
+// if (method_exists(\core_h5p\output\renderer::class, 'h5p_alter_styles')) {
+//   class theme_edumy_core_h5p_renderer extends \core_h5p\output\renderer {
+//     public function h5p_alter_styles(&$scripts, array $libraries, string $embedtype) {
+//       // global $PAGE;
+//       // $CSS = $PAGE->theme->get_setting('custom_css_h5p');
+//       // if (!empty($CSS)) {
+//           // $scripts[] = (object) array(
+//           //     'path' => 'https://demo.createdbycocoon.com/moodle/edumy/splash/asset/style.css',
+//           //     'version' => ''
+//           // );
+//       // }
+//     }
+//   }
+//   }
+// }
